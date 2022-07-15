47961d318ef16130e170b222a7261d703f4949e4
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-15 10:02:29 +0900

    ArrayList size  함수 구현

---
+++ src/main/java/ArrayList.java
@@ -0,0 +1,19 @@
+public class ArrayList {
+    private int size;
+
+    public ArrayList() {
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(int data) {
+        size++;
+    }
+
+    public int get(int index) {
+        return 100;
+    }
+}
