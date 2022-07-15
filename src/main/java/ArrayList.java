51352aa34dd440068c2a2694b75613a86c261625
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-15 12:59:45 +0900

    수정

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -32,9 +32,10 @@ public class ArrayList {
 
 
     public void removeAt(int index) {
-        for(int i = index + 1; i < datum.length; i++) {
+        for(int i = index + 1; i < size; i++) {
             datum[i-1] = datum[i];
         }
+
         size--;
     }
 
